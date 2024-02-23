c2f17163a8629837e8a2a8e6b2094600d1e49317
asayanlar <andrewsayanlar@gmail.com>
2024-02-23 14:26:55 -0500

    added/cleaned up comments
    removed unused code and print statements

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
+# from IPython.display import display
+import string
+from pathlib import Path
+# import re
 import pandas as pd
-from IPython.display import display
 import matplotlib.pyplot as plt 
 import matplotlib as mpl
 from matplotlib.gridspec import GridSpec
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
 from nltk.corpus import stopwords
-import string
 from wordcloud import WordCloud
-import string
-import re
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import sklearn.metrics as metrics
+from sklearn.metrics import classification_report
 from scipy.sparse import hstack
+import joblib
+
+nltk.download('stopwords')
+nltk.download('punkt')
 
 mpl.use('tkagg')
+
 cmap = plt.get_cmap('coolwarm')
 
 resume_data_set = pd.read_csv('data_set/UpdatedResumeDataSet.csv', encoding='utf-8')
+
 print(resume_data_set.head())
 print(resume_data_set.info())
 
@@ -40,52 +48,44 @@ axs[1].plot(title='CATEGORY DISTRIBUTION')
 axs[1].pie(num_same_category, labels=categories_list, autopct='%1.1f%%', shadow=True)
 
 plt.tight_layout()
-# plt.show()
+plt.show()
 
 # create a extra column for the cleaned up resume data so we can use it for our data set
-resume_data_set['cleaned_resume'] = ''
+resume_data_set['Cleaned Resumed'] = ''
 print(resume_data_set.head())
 print(resume_data_set.info())
 
-# print(''.join(word.strip(string.punctuation) for word in resume_data_set.at[0, 'Resume']))
-
-# for word in resume_data_set.at[0, 'Resume']: 
-#     char_index = resume_data_set.at[0, 'Resume'].index(word)
-#     if resume_data_set.at[0, 'Resume'][char_index + 1] != ' ':
-#         print(' '.join(word.strip(string.punctuation)))
-#     else:
-#         print(''.join(word.strip(string.punctuation)))
+#retrieve all resumes from resume column
 resume = resume_data_set['Resume']
-resume_data_set['cleaned_resume'] = resume.str.replace(r'[^a-zA-Z0-9\' ]+', ' ', regex=True).str.replace(r'[\s\s]+', ' ', regex=True)
 
-# print(resume_data_set.at[0, 'cleaned_resume'])
-# print(resume_data_set.head())
-# for x in range (0, 30):
-#     print(resume_data_set['cleaned_resume'][x])
-# print(resume_data_set.info())
+# remove any non-alphanumerical characters and double spaces due to the position of the removed
+# characters from the resume row and copy it over to a new column called Cleaned Resume
+resume_data_set['Cleaned Resumed'] = resume.str.replace(r'[^a-zA-Z0-9\' ]+', ' ', regex=True).str.replace(r'[\s\s]+', ' ', regex=True)
 
-resume_data_set_copy=resume_data_set.copy()
-
+# define the stop words not wanted in the resume column. This is since stop words are
+# insignificant because of how frequent they appear
 stop_words_set = set(stopwords.words('english')+['``',"''"])
 
 total_words =[]
-
 str_all_resumes = []
-for resume in resume_data_set['cleaned_resume']:
-    str_all_resumes += [resume]
 
+#put all strings from each row of the Cleaned Resume column into a single string
+for resume in resume_data_set['Cleaned Resumed']:
+    str_all_resumes += [resume]
 str_all_resumes = ''.join(str_all_resumes)
 
-token_resume_words = nltk.word_tokenize(''.join(str_all_resumes))
+# tokenize all words in the cleaned resume
+tokenize_resume_words = nltk.word_tokenize(str_all_resumes)
 
-for word in token_resume_words:
-    if word not in stop_words_set and word not in string.punctuation:
+# within those tokens remove all stop words
+for word in tokenize_resume_words:
+    if word not in stop_words_set:
         total_words.append(word)
 
+# Frequency distribution for all tokens found in all cleaned resumes
 word_freq_dist = nltk.FreqDist(total_words)
 
 most_common_words = word_freq_dist.most_common(50)
-print(most_common_words)
 
 wc = WordCloud().generate(str_all_resumes)
 plt.figure(figsize=(10,10))
@@ -93,10 +93,6 @@ plt.imshow(wc, interpolation='bilinear')
 plt.axis("off")
 plt.show()
 
-var_mod = ['Category']
+# Label the resumes based on their category using a numerical label
 le = LabelEncoder()
 resume_data_set['Category'] = le.fit_transform(resume_data_set['Category'])
-
-# print(resume_data_set.Category.value_counts())
-# print(resume_data_set_copy.Category.value_counts())
-del resume_data_set_copy
